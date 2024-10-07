5e28c0baea0adaf6cf19a885727aba9bb2a90896
TatianaPLC <tatianalizarazoc@hotmail.com>
2024-10-07 07:56:28 -0500

    Practicando el DOM

---
+++ DOM/app.js
@@ -0,0 +1,27 @@
+//let elemento = document;
+//elemento = document.head;
+//console.log(elemento);
+
+//let elementoW = window;
+//console.log(`Elementos de Windows ${elementoW}`);
+
+//let elementoE = document.styleSheets;
+//console.log(elementoE);
+
+//let elementoId = document.getElementById('tituloDOM');
+//console.log(elementoId);
+
+//Asignar un valor en el front con innerHTML
+let opId = document.getElementById('p1');
+opId.innerHTML = 'JavaScript';
+
+//Asignar un valor en el front con innerHTML
+let opId1 = document.getElementById('p2');
+opId1.innerHTML = 'Kotlin';
+
+//Asignar un valor en el front con innerHTML
+let opId2 = document.getElementById('p3');
+opId2.innerHTML = 'c++';
+
+let elementoClase = document.getElementsByClassName('lista');
+console.log(elementoClase);
